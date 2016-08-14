98e9b1b892c2428c61cafec18a3f9d45c115df42
®γσ, Eng (白戸則道) <englianhu@gmail.com>
2016-08-14 18:52:13 +0900

    updated, seperated paper due to memory limit.

--- function/libs.R
+++ function/libs.R
@@ -25,7 +25,8 @@ pkgs <- c('devtools', 'zoo', 'chron', 'stringr', 'reshape', 'reshape2', 'data.ta
           'stringdist', 'DT', 'plyr', 'dplyr', 'purrr', 'magrittr', 'foreach', 'manipulate', 'ggplot2', 
           'ggthemes', 'proto', 'extrafont', 'directlabels', 'PerformanceAnalytics', 'plotly', 'rstan', 
           'doParallel', 'rvest', 'highlightHTML', 'knitr', 'rmarkdown', 'scales', 'lubridate', 'tidyr', 
-          'whisker', 'gtable', 'grid', 'gridExtra', 'arules', 'arulesViz', 'googleVis', 'formattable')
+          'whisker', 'gtable', 'grid', 'gridExtra', 'arules', 'arulesViz', 'googleVis', 'formattable', 
+          'rCharts')
 #'@ c('memoise', 'RStudioAMI', 'pander', 'parallel', 'sqldf', 'BiocParallel', 'RSelenium', 
 #'@   'doMC', 'editR') #load if needed
 #'@ suppressAll(lib(pkgs)
